f6266e4991415f3294cc0c983fd108e6acb4d173
Daniel Mattox <dmattox10@gmail.com>
2020-02-21 12:31:47 -0500

    router

--- projects/product-router.js
+++ projects/product-router.js
@@ -4,6 +4,64 @@ const Projects = require('./project-model')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    Projects.findProjects()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
 
+router.post('/', (req, res) => {
+    Projects.insertProject(req.body)
+    .then(id => {
+        res.status(201).json(id)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+router.get('/resources', (req, res) => {
+    Projects.findRes()
+    .then(resources => {
+        res.status(200).json(resources)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+router.post('/resources', (req, res) => {
+    Projects.insertRes(req.body)
+    .then(id => {
+        res.status(200).json(id)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+router.get('/tasks/:id', (req, res) => {
+    Projects.findTasks(req.params.id)
+    .then(tasks => {
+        res.status(200).json(tasks)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+router.post('/tasks', (req, res) => {
+    Projects.insertTask(req.body)
+    .then(id => {
+        res.status(200).json(id)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
 
 module.exports = router
